af33c49b0c1f07e7c9eefd679cb32ea49d4ae202
Arda Cihat Karahan <ardacihatkarahan@gmail.com>
2024-08-01 00:05:33 +0300

    Added some game mechanics

--- Assets/Scripts/ZombieMovement.cs
+++ Assets/Scripts/ZombieMovement.cs
@@ -6,6 +6,10 @@ using UnityEngine.AI;
 public class ZombieMovement : MonoBehaviour
 {
     private GameObject players;
+    private int zombiehealth = 3;
+    private float distencee;
+    public GameObject heart;
+     
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +20,24 @@ public class ZombieMovement : MonoBehaviour
     void Update()
     {
         GetComponent<NavMeshAgent>().destination = players.transform.position;
+        distencee = Vector3.Distance(transform.position, players.transform.position);
+        if(distencee < 3f)
+        {
+            GetComponentInChildren<Animation>().Play("Zombie_Attack_01");
+        }
+    }
+    private void OnCollisionEnter(Collision c)
+    {
+        if (c.collider.gameObject.tag.Equals("bullet"))
+        {
+            Debug.Log("vurdun");
+            zombiehealth--;
+            if (zombiehealth == 1)
+            {
+                Instantiate(heart, transform.position,Quaternion.identity);
+                GetComponentInChildren<Animation>().Play("Zombie_Death_01");
+                Destroy(this.gameObject, 1.667f);
+            }
+        }
     }
 }
--- Assets/Scripts/gun.cs
+++ Assets/Scripts/gun.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class gun : MonoBehaviour
 {
     public Transform ammoPos;
     public GameObject bullet;
     public GameObject Explosion;
+    public Image healthImagine;
+    private float healthValue = 100f;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,4 +28,26 @@ public class gun : MonoBehaviour
             Destroy(goExplosion.gameObject, 2f);
         }
     }
+    private void OnCollisionEnter(Collision c)
+    {
+        if (c.collider.gameObject.tag.Equals("zombi"))
+        {
+            healthValue -= 10f;
+            float x = healthValue / 100f;
+            healthImagine.fillAmount = x;
+            healthImagine.color=Color.Lerp(Color.red,Color.green,x);    
+        }
+    }
+    private void OnTriggerEnter(Collider c)
+    {
+        if (c.gameObject.tag.Equals("heart"))
+        {
+            healthValue += 10f;
+            Destroy(c.gameObject);
+            float x = healthValue / 100f;
+            healthImagine.fillAmount = x;
+            healthImagine.color = Color.Lerp(Color.red, Color.green, x);
+        }
+    }
+
 }
